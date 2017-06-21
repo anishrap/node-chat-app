a16cf0dc0709f8bc466ffebff398a24e21740f39
Anish Raparla <anishrap@umich.edu>
2017-06-21 17:56:36 +0530

    Included message generator and tests

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -15,24 +16,14 @@ app.use(express.static(publicPath));
 io.on('connection', function (socket) {
   console.log('New User Connected!');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the Chat App!'
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to Chat App'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New User Joined'
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
 
   socket.on('createMessage', function (message) {
     console.log('created Message', message);
 
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', function () {
