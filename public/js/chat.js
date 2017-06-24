eb5b0a8c661547b5355b3c609cc16d76b7470191
Anish Raparla <anishrap@umich.edu>
2017-06-24 22:30:05 +0530

    Added messages only to user room

--- public/js/chat.js
+++ public/js/chat.js
@@ -33,6 +33,7 @@ socket.on('connect', function () {
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
   var template = jQuery('#message-template').html();
+  console.log(message);
   var html = Mustache.render(template, {
     text: message.text,
     from: message.from,
@@ -97,7 +98,6 @@ jQuery('#message-form').on('submit', function(e) {
   var messageTextBox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextBox.val()
   }, function () {
       messageTextBox.val('');
